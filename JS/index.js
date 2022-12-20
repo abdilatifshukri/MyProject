ee7c156f722aeb2c0ba08a04935bb4a5c914a3dc
abdilatifshukri <abdilatif699@gmail.com>
2022-12-20 21:35:30 +0300

    completed this

--- JS/index.js
+++ JS/index.js
@@ -1,152 +1,91 @@
-document.addEventListener('DOMContentLoaded', () => {
+let timer
+let deleteFirstPhotoDelay
 
-    function fetchData(){
-        fetch('https://api.npoint.io/e63965b6eea20ecdb285')
-        .then(res => res.json())
-        .then(data => renderPlayers(data))
+async function start() {
+  try {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    const data = await response.json()
+    createBreedList(data.message)
+  } catch (e) {
+    console.log("There was a problem fetching the breed list.")
+  }
+}
+
+start()
+
+function createBreedList(breedList) {
+  document.getElementById("breed").innerHTML = `
+  <select onchange="loadByBreed(this.value)">
+        <option>Choose a dog breed</option>
+        ${Object.keys(breedList).map(function (breed) {
+          return `<option>${breed}</option>`
+        }).join('')}
+      </select>
+  `
+}
+
+async function loadByBreed(breed) {
+  if (breed != "Choose a dog breed") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+    const data = await response.json()
+    createSlideshow(data.message)
+  }
+}
+
+function createSlideshow(images) {
+  let currentPosition = 0
+  clearInterval(timer)
+  clearTimeout(deleteFirstPhotoDelay)
+  
+  if (images.length > 1) {
+    document.getElementById("slideshow").innerHTML = `
+  <div class="slide" style="background-image: url('${images[0]}')"></div>
+  <div class="slide" style="background-image: url('${images[1]}')"></div>
+  `
+  currentPosition += 2
+  if (images.length == 2) currentPosition = 0
+  timer = setInterval(nextSlide, 3000)
+  } else {
+    document.getElementById("slideshow").innerHTML = `
+  <div class="slide" style="background-image: url('${images[0]}')"></div>
+  <div class="slide"></div>
+  `
+  }
+
+  function nextSlide() {
+    document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
+    deleteFirstPhotoDelay = setTimeout(function () {
+      document.querySelector(".slide").remove()
+    }, 1000)
+    if (currentPosition + 1 >= images.length) {
+      currentPosition = 0
+    } else {
+      currentPosition++
     }
-    
-    const body = document.body
+  }
+}
+ 
+ 
 
-    const playerFrame = document.createElement('div');
-    playerFrame.classList.add('frame');
+ // Login page DOM manupulation
+ const login = document.getElementById('login-btn');
+ const loginDiv = document.getElementById('login-div');
+ login.addEventListener('click', () => {
+   blog.remove();
+   video.remove();
+   li1.remove();
+   li2.remove();
+   login.appendChild(loginDiv);
+   body.appendChild(loginDiv)
+ })
+ const loginBtn = document.getElementById('login');
+ loginBtn.addEventListener('click', () => {
+   loginDiv.remove();
 
-    const voteFrame = document.createElement('div');
-    voteFrame.classList.add('vote-frame');
-
-    const searchFrame = document.createElement('div');
-    searchFrame.classList.add('search-frame');
-
-    const blog = document.getElementById('blog');
-    const video = document.getElementById('video');
-
-    const li1 = document.getElementById('players-list');
-    const li2 = document.getElementById('voting');
-
-
-
-
-    function renderPlayers(data){
-        
-        
-        data.forEach( player => {
-        const playerCard = document.createElement('div');
-        playerCard.classList.add('player-card');
-        playerCard.innerHTML = `
-           <img class="player-image" src="${player.Image}"/>
-           <h3 class="player-name font">${player.Name}</h3>
-           <h5 class="player-age font">Age: ${player.Age}</h5>
-           <h5 class="player-position font">Position: ${player.Position}</h5>
-           <h5 class="player-goals font">Goals: ${player.Goals}</h5> 
-           <h5 class="player-assist font">Assist: ${player.Assist}</h5>
-        `
-        playerFrame.appendChild(playerCard)
-
-        
-        
-        li1.addEventListener('click', () => {
-            blog.remove();
-            video.remove();
-            loginDiv.remove();
-            voteFrame.remove();
-            li1.appendChild(playerFrame);
-            body.appendChild(li1)
-        })
-
-        
-        
-        
-        
-        });
-    }
-    
-    
-    function fetchAwardData(){
-        fetch('https://api.npoint.io/e63965b6eea20ecdb285')
-        .then(res => res.json())
-        .then(data => renderAwards(data))
-    } 
-        
-        
-        
-        
-        function renderAwards(data){
-            
-            const voteDiv = document.getElementById('vote-header')
-            voteFrame.appendChild(voteDiv)
-            const newData = data.slice(0,4);
-            newData.forEach( vote => {
-                const voteCard = document.createElement('div');
-            voteCard.classList.add('vote-card');
-            voteCard.innerHTML = `
-           <img class="player-image" src="${vote.Image}"/>
-           <h3 class="player-name font">${vote.Name}</h3>
-           <h5 class="player-age font">Age: ${vote.Age}</h5>
-           <h5 class="player-position font">Position: ${vote.Position}</h5>
-           <h5 class="player-goals font">Goals: ${vote.Goals}</h5> 
-           <h5 class="player-assist font">Assist: ${vote.Assist}</h5>
-        `;
-        voteFrame.appendChild(voteCard)
-
-        
-        const tally = document.createElement('div');
-        tally.classList.add('tally');
-        tally.innerHTML = 0;
-        voteCard.appendChild(tally);
-        
-        const btn = document.createElement('button');
-        btn.classList.add('vote-btn', 'font');
-        btn.textContent = 'VOTE';
-        
-        btn.addEventListener('click', () => {
-            tally.innerHTML++;
-        });
-        voteCard.appendChild(btn);
-
-        li2.addEventListener('click', () => {
-            blog.remove();
-            video.remove();
-            loginDiv.remove();
-            playerFrame.remove();
-            li2.appendChild(voteFrame);
-            body.appendChild(li2);
-            });
-
-        })
-    
-        
-    
-    }
-
-    // Login page DOM manupulation
-    const login = document.getElementById('login-btn');
-    const loginDiv = document.getElementById('login-div');
-    login.addEventListener('click', () => {
-      blog.remove();
-      video.remove();
-      li1.remove();
-      li2.remove();
-      login.appendChild(loginDiv);
-      body.appendChild(loginDiv)
-    })
-    const loginBtn = document.getElementById('login');
-    loginBtn.addEventListener('click', () => {
-      loginDiv.remove();
-
-      body.appendChild(li1)
-      body.appendChild(li2)
-      body.appendChild(blog);
-      body.appendChild(video)
-      
-    });
-
-
-    
-
-
-      
-    
-    fetchAwardData()
-    fetchData()
-    
-})
+   body.appendChild(li1)
+   body.appendChild(li2)
+   body.appendChild(blog);
+   body.appendChild(video)
+   
+ });
+ 
--- README.md
+++ README.md
@@ -1,22 +1,48 @@
-# MyProject
- https://abdilatifshukri.github.io/MyProject/
- {
-    "name": "MYPRO",
-    "version": "1.0.0",
-    "description": "Mancity",
-    "main": "index.js",
-    "scripts": {
-      "test": "echo \"Error: no test specified\" && exit 1"
-    },
-    "repository": {
-      "type": "git",
-      "url": "git+https://github.com/AbdiazizAbdullahi/The-Champions.git"
-    },
-    "author": "Abdiaziz Abdullahi",
-    "license": "ISC",
-    "bugs": {
-      "url": "https://github.com/AbdiazizAbdullahi/The-Champions/issues"
-    },
-    "homepage": "https://github.com/AbdiazizAbdullahi/The-Champions#readme"
-  }
-  
+# MYPROJECT
+
+## DESCRIPTION
+MY PROJECT is a web application for voting  players
+<br>
+<br>
+
+### INSTALLATION
+
+To install the repository on your machine follow the instructions below;
+
+- Open a terminal on your computer.
+- Clone this repo to your machine using the ssh key provided:-
+
+      https://github.com/abdilatifshukri/MyProject
+
+- Change directory to the code challenge folder.     
+
+     cd MyProject
+        
+- Open it in a text editor of your choice.
+
+## PROJECT SETUP.
+The app fetches data from a puplic api  . if you open the app you will see a select or  a dropdown element that says choose  a dog breed and then if you select one it showes you a slideshow of the images of the dog breed that you slected
+
+# RUNNING THE APPLICATION
+To run the app on your machine;
+
+- Open the html file in the browser of you choice.
+
+- at first you will see a screen with simple dog pic ture with some text
+
+- when you scroll down you will see  a select or a 
+dropdown element 
+
+- if you click on chose dog breed you will see a list of dog breeds 
+
+- choose the dog breed you want and you will see a slideshow of its pictures
+
+# AUTHOR.
+This project was done by:
+abdilatif shukri
+
+# LICENSE.
+This project is under license Apache 2.0
+
+# live link.
+ tangerine-biscuit-4628c1.netlify.app
